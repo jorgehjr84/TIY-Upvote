4f8339f72240a283de05270f8295847535a5f241
Sean Joyal <sean.joyal@gmail.com>
2015-06-30 15:35:03 -0400

    wiring up ngRoute, created multipe route params

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+(function(window){
+
+  var app = angular.module('tiy-upvote', [ 'ngRoute', 'restangular' ]);
+
+  app.config(function($routeProvider){
+    $routeProvider.when('/questions' {
+      templateUrl: 'views/questions.html',
+      controller: 'QuestionsController',
+      controllerAs: 'question'
+    });
+
+    $routeProvider.when('/404' {
+      templateUrl: 'views/404.html'
+    });
+
+    $routeProvider.otherwise('/questions');
+  });
+
+
+
+
+
+})(window);
