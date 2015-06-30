6006796cf054d9d04ade3aa782908b3c7f97ee18
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-30 16:39:01 -0400

    added firebase, still trying to PUT data into the firbase app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+var app = angular.module('tiy-upvote', ['ngRoute', 'restangular']);
+
+var test = {
+  name: 'Jessy',
+  name2: 'Jorge'
+};
+
+app.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('https://gatorpazz-tiy-upvote.firebaseio.com/');
+});
+
+
+app.controller('MainController', function($route, Restangular) {
+  Restangular.one('name.json').put('test');
+});
