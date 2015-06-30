4d05ed890bf26b943e07c0ab693667d9ea2f5d50
Sean Joyal <sean.joyal@gmail.com>
2015-06-30 17:25:08 -0400

    added additional html pages, wired into ngRoute

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,18 @@
       controllerAs: 'question'
     });
 
+    $routeProvider.when('/selected', {
+      templateUrl: 'views/selected.html',
+      controller: 'ViewQuestionController',
+      controllerAs: 'selectedQuestion'
+    });
+
+    $routeProvider.when('/submit', {
+      templateUrl: 'views/submit.html',
+      controller: 'SubmitController',
+      controllerAs: 'submission'
+    });
+
     $routeProvider.when('/404', {
       templateUrl: 'views/404.html'
     });
@@ -16,11 +28,11 @@
     $routeProvider.otherwise('/questions');
   });
 
-  app.controller('QuestionsController', function($routeParams, Restangular){
+  app.controller('QuestionsController', function(Restangular){
     var self = this;
 
     Restangular.setBaseUrl('https://gatorpazz-tiy-upvote.firebaseio.com')
-      .one('user', 'username').put('gatorpazz');
+      .one('user').put('{ "username": "gatorpazz" }');
   });
 
 
