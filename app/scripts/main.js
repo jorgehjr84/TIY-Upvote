f5233e67fc03546ab2b57f2222552286ebbf84dc
Sean Joyal <sean.joyal@gmail.com>
2015-06-30 16:10:38 -0400

    shits on the page, ngRoute is working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,18 +3,24 @@
   var app = angular.module('tiy-upvote', [ 'ngRoute', 'restangular' ]);
 
   app.config(function($routeProvider){
-    $routeProvider.when('/questions' {
+    $routeProvider.when('/questions', {
       templateUrl: 'views/questions.html',
       controller: 'QuestionsController',
       controllerAs: 'question'
     });
 
-    $routeProvider.when('/404' {
+    $routeProvider.when('/404', {
       templateUrl: 'views/404.html'
     });
 
     $routeProvider.otherwise('/questions');
   });
+
+  app.controller('QuestionsController', function($routeParams, Restangular){
+
+  });
+
+
 
 
 
