bdd98262ce5e71f223e8ea8b23cab1608b9bbb4e
Sean Joyal <sean.joyal@gmail.com>
2015-07-02 16:06:05 -0400

    added questions controller, removed var app references

--- app/scripts/tiy-upvote.js
+++ app/scripts/tiy-upvote.js
@@ -2,46 +2,45 @@
 (function(){
 'use strict';
 
-  var app = angular.module('tiy-upvote', [ 'ngRoute', 'restangular' ]);
+  angular.module('tiy-upvote', [ 'ngRoute', 'restangular' ])
+    .config(function($routeProvider){
+      $routeProvider.when('/questions', {
+        templateUrl: 'views/questions.html',
+        controller: 'QuestionsController',
+        controllerAs: 'question'
+      });
 
-  app.config(function($routeProvider){
-    $routeProvider.when('/questions', {
-      templateUrl: 'views/questions.html',
-      controller: 'QuestionsController',
-      controllerAs: 'question'
+      $routeProvider.when('/users/:user', {
+        templateUrl: 'views/user.html'
+        // controller: 'UsersController',
+        // controllerAs: 'user'
+      });
+
+      $routeProvider.when('/questions/ask', {
+        templateUrl: 'views/submit.html'
+        // controller: 'SubmitController',
+        // controllerAs: 'submission'
+      });
+
+      $routeProvider.when('/questions/:question', {
+        templateUrl: 'views/selected.html'
+        // controller: 'ViewQuestionController',
+        // controllerAs: 'selectedQuestion'
+      });
+
+      $routeProvider.when('/404', {
+        templateUrl: 'views/404.html'
+      });
+
+      $routeProvider.when('/', {
+        redirectTo: '/questions'
+      });
+
+      $routeProvider.otherwise('/404');
     });
 
-    $routeProvider.when('/users/:user', {
-      templateUrl: 'views/user.html'
-      // controller: 'UsersController',
-      // controllerAs: 'user'
+      .config(function(RestangularProvider){
+        RestangularProvider.setBaseUrl('https://gatorpazz-tiy-upvote.firebaseio.com');
     });
-
-    $routeProvider.when('/questions/ask', {
-      templateUrl: 'views/submit.html'
-      // controller: 'SubmitController',
-      // controllerAs: 'submission'
-    });
-
-    $routeProvider.when('/questions/:question', {
-      templateUrl: 'views/selected.html'
-      // controller: 'ViewQuestionController',
-      // controllerAs: 'selectedQuestion'
-    });
-
-    $routeProvider.when('/404', {
-      templateUrl: 'views/404.html'
-    });
-
-    $routeProvider.when('/', {
-      redirectTo: '/questions'
-    });
-
-    $routeProvider.otherwise('/404');
-  });
-
-  app.config(function(RestangularProvider){
-    RestangularProvider.setBaseUrl('https://gatorpazz-tiy-upvote.firebaseio.com');
-  });
 
 })(window);
--- app/scripts/tiy-upvote/questions.controller.js
+++ app/scripts/tiy-upvote/questions.controller.js
@@ -1,9 +1,9 @@
 (function(){
 
-  app.controller('QuestionsController', function(){
-    // var self = this;
-    // Restangular
-    //   .one('user').put('{ "username": "gatorpazz" }');
-  });
-
-})
+  angular.module('tiy-upvote')
+    .controller('QuestionsController', function(){
+      // var self = this;
+      // Restangular
+      //   .one('user').put('{ "username": "gatorpazz" }');
+    });
+})();
