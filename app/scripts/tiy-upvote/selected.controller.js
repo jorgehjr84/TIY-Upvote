7146cd331e62436c8c4caadf92243376729232ab
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-03 12:13:45 -0400

    Did some work on the selectedController, stuff shows up

--- app/scripts/tiy-upvote/selected.controller.js
+++ app/scripts/tiy-upvote/selected.controller.js
@@ -1,30 +1,16 @@
 (function(){
 
   angular.module('tiy-upvote')
-    .controller('SelectedController', function(){
-      // this.question = {
-      //   "synopsis": "How do I do this?",
-      //   "body": "I really don't know how to do this, how do I do this?",
-      //   "created_by": "sjoyal",
-      //   "timestamp": "",
-      //   "answers": [
-      //     {
-      //       "body": "LOL NEWB",
-      //       "created_by": "trollbot1",
-      //       "timestamp": ""
-      //     },
-      //     {
-      //       "body": "YOU SUCK BRAH!!!!",
-      //       "created_by": "trollbot2",
-      //       "timestamp": ""
-      //     },
-      //     {
-      //       "body": "What have you tried??? HAVE YOU TRIED ANYTHING?!!??! WE'RE NOT YOUR PRIVATE ARMY!!!!!!!",
-      //       "created_by": "anon",
-      //       "timestamp": ""
-      //     }
-      //   ]
-      // };
+    .controller('SelectedController', function($http){
+      var self = this;
+      var inquiry = [];
 
-    });
+      $http.get('/api/gatorpazz-tiy-upvote/questions/dummyData.json')
+        .then(function(response) {
+          console.log(response.data);
+          self.inquiry = response.data;
+        });
+      });
+
+
 })();
