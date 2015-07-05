a217d841869e1c5cf2cd9948e7cff75970404a5c
Sean Joyal <sean.joyal@gmail.com>
2015-07-05 17:31:10 -0400

    black magic with a createAnswer method that works and stuff

--- app/scripts/tiy-upvote/selected.controller.js
+++ app/scripts/tiy-upvote/selected.controller.js
@@ -6,6 +6,14 @@
     .controller('SelectedController', function($scope, $routeParams, Question) {
       console.log($routeParams);
       $scope.question = Question.get($routeParams.questionId);
+      $scope.answer = {
+        'created_by': '',
+        'body': ''
+      };
+
+      $scope.submitAnswer = function() {
+        Question.createAnswer($routeParams.questionId, $scope.answer);
+      };
     });
 
 })();
